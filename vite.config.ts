d4b13e4fb53769b7ed11e632a55c266f51d3ffde
Ayush Saxena <127773590+Ayushsaxena38@users.noreply.github.com>
2025-09-26 15:41:20 +0530

    Update vite.config.ts
    updated vite config and added preview allowed all

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react() , tailwindcss()],
+// })
+
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-})
+  plugins: [react()],
+  preview: {
+    allowedHosts: ['kanban-board-2-m7hi.onrender.com'], // add your Render host here
+    port: 10000, // optional, match your package.json start script
+    host: '0.0.0.0',
+  },
+});
